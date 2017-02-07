ecf654e14eb0fdf9741ea0a4bd8ded6f2ecf3c2d
Takehime <silviapimpaovasquez@hotmail.com>
2017-02-07 01:19:39 -0200

    Coleta de power ups
    - coleta de power ups feita com linecast
    - mensagem amigavel e motivadora temporaria para quando se coleta o
    power up

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,21 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public Transform sightStart, sightEndXY, sightEnd_XY, sightEndX_Y, sightEnd_X_Y;
+    public Text powerUpColetado;
+    public int relacaoScoreToPowerUpText = 500;
+
+    private bool spottedXY = false, spotted_XY = false, spottedX_Y = false, spotted_X_Y = false;
+    private GameObject HUD;
+    private GameObject score;
+
+    void Start()
+    {
+        HUD = GameObject.FindGameObjectWithTag("HUD");
+        score = GameObject.FindGameObjectWithTag("Score");
+    }
+
+    void Update ()
+    {
+        raycast();
+        collectPowerUp();
 	}
 
-    public void destroyPowerUp()
+    void destroyPowerUp()
     {
         Destroy(this.gameObject);
+    }
+
+    void raycast()
+    {
+        Debug.DrawLine(sightStart.position, sightEndXY.position, Color.magenta);
+        Debug.DrawLine(sightStart.position, sightEnd_XY.position, Color.red);
+        Debug.DrawLine(sightStart.position, sightEndX_Y.position, Color.blue);
+        Debug.DrawLine(sightStart.position, sightEnd_X_Y.position, Color.yellow);
+
+        spottedXY = Physics2D.Linecast(sightStart.position, sightEndXY.position, 1 << LayerMask.NameToLayer("Escada"));
+        spotted_XY = Physics2D.Linecast(sightStart.position, sightEnd_XY.position, 1 << LayerMask.NameToLayer("Escada"));
+        spottedX_Y = Physics2D.Linecast(sightStart.position, sightEndX_Y.position, 1 << LayerMask.NameToLayer("Escada"));
+        spotted_X_Y = Physics2D.Linecast(sightStart.position, sightEnd_X_Y.position, 1 << LayerMask.NameToLayer("Escada"));
+    }
+
+    void collectPowerUp()
+    {
+        if(spottedXY && spottedX_Y && spotted_XY && spotted_X_Y)
+        {
+            GameObject temp = Instantiate(powerUpColetado.gameObject);
+            temp.transform.localPosition = new Vector3(0, score.transform.position.y- relacaoScoreToPowerUpText, 0);
+            temp.transform.SetParent(HUD.transform, false);
+            destroyPowerUp();
+        }
     }
 }
