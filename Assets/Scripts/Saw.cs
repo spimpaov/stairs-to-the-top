95c57919793688eb60ae72ba1e0286cddbb07684
Pedro Nascimento <pedrovitor.marques@gmail.com>
2017-07-12 08:38:09 -0300

    Algumas substituições de sprite, etc
    *Cupim com os bichinhos se movendo loucamente.
    *Mudança do logo da GDP no menu principal

--- Assets/Scripts/Saw.cs
+++ Assets/Scripts/Saw.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class Saw : MonoBehaviour {
 
+    [SerializeField] private GameObject brokenSawPrefab;
+
     public void destroySaw()
     {
+        GameObject go;
         Destroy(this.gameObject);
+        go = Instantiate(brokenSawPrefab,transform.position,Quaternion.identity);
+        go.GetComponent<Rigidbody2D>().AddForce(Vector2.up*500);
+        go.transform.Rotate(0,0,Random.Range(0,90));
     }
 }
