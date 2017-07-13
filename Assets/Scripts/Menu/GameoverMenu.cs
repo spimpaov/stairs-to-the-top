0d7539db6b790aaf825ea6484cdd50f7f5f82cc9
Pedro Nascimento <pedrovitor.marques@gmail.com>
2017-07-13 11:23:26 -0300

    Mudança na tela de GameOver

---
+++ Assets/Scripts/Menu/GameoverMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameoverMenu : MonoBehaviour {
+
+	[SerializeField] private Transform rightPosition;
+	[SerializeField] private float speed;
+	[SerializeField] private List<GameObject> uiObjects;
+
+	private void Start(){
+		StartCoroutine( Up() );
+		DesactivateUI();
+	}
+	private IEnumerator Up(){
+		while(this.transform.position.y < rightPosition.position.y){
+			transform.position += Vector3.up*Time.deltaTime*speed;
+			yield return new WaitForEndOfFrame();
+		}
+		ActivateUI();
+	}
+	private void  ActivateUI(){
+		foreach(GameObject go in uiObjects){
+			go.SetActive(true);
+		}
+	}
+	private void  DesactivateUI(){
+		foreach(GameObject go in uiObjects){
+			go.SetActive(false);
+		}
+	}
+}
