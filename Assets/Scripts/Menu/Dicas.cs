1893a1546fe42b40084f5e11951ddedcfffaca74
Pedro Nascimento <pedrovitor.marques@gmail.com>
2017-07-18 08:46:43 -0300

    v1.1
    OK - Colocar créditos no stairs, em especial pelas músicas
    OK - Dizer que se joga com swipe
    OK - Tutorial não ficou claro sobre os power-ups

---
+++ Assets/Scripts/Menu/Dicas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dicas : MonoBehaviour {
+
+	[SerializeField]private List<string> dicas;
+
+	private void Start(){
+		GetComponent<Text>().text = "<b>DICA</b>\n" + dicas[ Random.Range(0,dicas.Count)];
+	}
+}
